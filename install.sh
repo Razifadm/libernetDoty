31be1b1770a85cd45e197d5e4204151f5ec8a098
raducksijaa <ronald.alan77@gmail.com>
2025-10-24 23:50:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ LIBERNET_WWW="/www/libernet"
 STATUS_LOG="${LIBERNET_DIR}/log/status.log"
 DOWNLOADS_DIR="${HOME}/Downloads"
 LIBERNET_TMP="${DOWNLOADS_DIR}/libernet"
-REPOSITORY_URL="https://github.com/dotywrt/libernet"
+REPOSITORY_URL="https://github.com/Razifadm/libernetDoty"
 
 function install_packages() {
   while IFS= read -r line; do
