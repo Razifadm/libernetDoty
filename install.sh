c2f60184566396459262f5f08759b124e5103fee
raducksijaa <ronald.alan77@gmail.com>
2025-10-27 08:06:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -174,7 +174,7 @@ function main_installer() {
     && configure_libernet_service \
     && setup_system_logs \
     && finish_install
-    && mkdir -p /usr/lib/lua/luci/view/libernet \
+    && mkdir -p /usr/lib/lua/luci/view/libernet
     && mv "${LIBERNET_TMP}/View/libernet.lua" /usr/lib/lua/luci/controller/libernet.lua \
     && mv "${LIBERNET_TMP}/View/iframe.htm" /usr/lib/lua/luci/view/libernet/iframe.htm \
 }
